0266d40c8bc4e3180bd0c081db00dd2e9ea5018b
sergioSHKLR <107352082+sergioSHKLR@users.noreply.github.com>
2026-09-21 09:11:22 -0300

    Add audio module loader and SW precache for split Hello MP3.

---
+++ hello-audio.js
@@ -0,0 +1,5 @@
+import a from './hello-audio-a.js';
+import b from './hello-audio-b.js';
+import c from './hello-audio-c.js';
+export const HELLO_AUDIO_MIME = 'audio/mpeg';
+export const HELLO_AUDIO_B64 = a + b + c;
--- sw.js
+++ sw.js
@@ -5,6 +5,9 @@ const PRECACHE = [
   './styles.css',
   './app.js',
   './hello-audio.js',
+  './hello-audio-a.js',
+  './hello-audio-b.js',
+  './hello-audio-c.js',
   './manifest.json',
   './icons/nose.svg',
 ];
